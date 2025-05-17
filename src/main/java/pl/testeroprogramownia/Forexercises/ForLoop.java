7dea378f6dc8829c0e4bce98af262d4da983c90b
Jed4h86 <miszapiek@gmail.com>
2025-05-17 19:10:31 +0200

    PrimeNumbers test

---
+++ src/main/java/pl/testeroprogramownia/Forexercises/ForLoop.java
@@ -0,0 +1,20 @@
+package pl.testeroprogramownia.Forexercises;
+
+import java.util.Scanner;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for(int i=0; i<=n; i++){
+            sum+=i;
+
+
+        }
+        System.out.println(sum);
+
+    }
+}
---
+++ src/main/java/pl/testeroprogramownia/Forexercises/StandardForLoop.java
@@ -0,0 +1,33 @@
+package pl.testeroprogramownia.Forexercises;
+
+public class StandardForLoop {
+    public static void main(String[] args) {
+        //wypisz cyfry od 0 do 10
+        System.out.println("wypisz cyfry od 0 do 10:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        //wyswietl liczby parzyste
+        System.out.println("wyswietl liczby parzyste:");
+        for (int j = 2; j < 10; j = j + 2) {
+            System.out.println(j);
+        }
+//wyswietl liczby nieparzyste
+        System.out.println("wyswietl liczby nieparzyste:");
+        for (int j = 2; j < 10; j = j + 2) {
+            System.out.println(j);
+
+    }
+        //wypisz cyfry od 10 do 0
+        System.out.println("wypisz cyfry od 10 do 0:");
+        for (int k = 10; k > 0; k--) {
+            System.out.println(k);
+        }
+        /// wypisz Stringi:
+        System.out.println("wypisz Stringi:");
+        for (int l = 0; l < 10; l++){
+            System.out.println(l + " Dostanę smaczków");
+        }
+
+    }
+}
