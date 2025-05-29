da6f082b540580f8b86608952e69d21b3ce57bc3
Jed4h86 <miszapiek@gmail.com>
2025-05-29 20:10:37 +0200

    Ćwiczenei metod z returnami

--- src/main/java/pl/testeroprogramownia/Bagerka/Działamy.java
+++ src/main/java/pl/testeroprogramownia/Bagerka/Działamy.java
@@ -4,16 +4,21 @@ package pl.testeroprogramownia.Bagerka;
 import static pl.testeroprogramownia.Bagerka.BageraDay.bageraActivities;
 import static pl.testeroprogramownia.Bagerka.Mruczando.miau;
 import static pl.testeroprogramownia.Bagerka.Mruczando.miau2;
+import static pl.testeroprogramownia.Bagerka.Return.kabanosyBagery;
+import static pl.testeroprogramownia.Bagerka.Return.mniejKabanosow;
 
 public class Działamy {
     public static void main(String[] args) {
         //z metodą statyczną
-        bageraActivities();
-        miau();
-        miau2();
+//        bageraActivities();
+//        miau();
+//        miau2();
 
         //bez metody statycznej
 //        BageraDay a = new BageraDay();
 //        a.bageraActivities();
+//kabanosyBagery(1,2);
+        System.out.println(kabanosyBagery(1,2));
+        System.out.println(mniejKabanosow(12,7));
     }
 }
---
+++ src/main/java/pl/testeroprogramownia/Bagerka/Return.java
@@ -0,0 +1,11 @@
+package pl.testeroprogramownia.Bagerka;
+
+public class Return {
+    public static int kabanosyBagery(int a, int b) {
+        return a+b;
+
+    }
+    public static int mniejKabanosow(int a,int b){
+        return a-b;
+    }
+}
